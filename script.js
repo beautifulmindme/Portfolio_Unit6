754216ba51a4eeb43c23ce4408d3dca2a002a2b8
Jane R <eldous@daum.net>
2017-03-14 02:59:39 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-    //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
-    });
-    
-    
-    
-    
+$(document).ready(function(){ // Code to run goes here
+
 });
